2eb2d1a2ef571274d723d0a9691d89ce90729264
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-19 13:05:27 -0400

    Dummy login function

--- friends/src/util/network.js
+++ friends/src/util/network.js
@@ -1,4 +1,12 @@
 let token;
+const LOGIN = "LOGIN";
+
+const initialState = {
+  loggedIn: false,
+  isLoggingIn: false,
+};
+
+export function login(username, password) {}
 
 export function inititalizeNetwork() {
   token = JSON.parse(localStorage.getItem("token"));
