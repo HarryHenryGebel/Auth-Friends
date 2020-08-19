0dbfdd4972490afb8ce846caa76ef89b8c17e984
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-19 11:55:24 -0400

    create network module

--- friends/src/index.jsx
+++ friends/src/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+
+import inititalizeNetwork from "./util/network";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ friends/src/util/network.js
@@ -0,0 +1,6 @@
+let token;
+
+export function inititalizeNetwork() {
+  token = JSON.parse(localStorage.getItem("token"));
+  debugger;
+}
