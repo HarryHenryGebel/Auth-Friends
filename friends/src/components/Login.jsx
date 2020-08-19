efebf79f628d3ada0b25ec523473197431ef84f6
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-19 14:55:26 -0400

    Enable button when password is filled in instead of when it's empty

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -53,7 +53,9 @@ export default function Login() {
         </Box>
         <Box m={1}>
           <Button
-            disabled={values.username.trim() === "" || values.password.trim()}
+            disabled={
+              values.username.trim() === "" || values.password.trim() === ""
+            }
             onClick={() => {
               login(values.username, values.password);
               setValues(initialValues);
